e2f43fda87687c8baec258d18d429d5bfa013fb3
lucioroadtoglory <lucioandradejr@gmail.com>
2022-10-03 08:47:39 -0300

    style: removing console log

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,6 @@ export default function Home() {
   }, []);
 
   async function loadResults() {
-    console.log("entrou aqui");
     const response = await fetch(
       "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json"
     ).then((res) => res.json());
